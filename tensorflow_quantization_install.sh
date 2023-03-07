6bdf60e22d1752ab7eda4b3b470cc26f1f2ab11b
vaik-info <info@vaikobo.com>
2023-03-07 16:39:28 +0900

    first commit

--- tensorflow_quantization_install.sh
+++ tensorflow_quantization_install.sh
@@ -7,4 +7,5 @@ trap 'rm -rf $temp_dir' EXIT
 cd $temp_dir
 git clone https://github.com/vaik-info/vaik-TensorRT.git
 cd ./vaik-TensorRT/tools/tensorflow-quantization
+pip install tf2onnx==1.12.1
 pip install . --no-dependencies
